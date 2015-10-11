c0cba251e8a09908d5861d1e29207d55afd54b2d
David Barnett <dbarnett@liquidweb.com>
2015-10-11 17:21:03 -0400

    decks page "done"

--- decks.php
+++ decks.php
@@ -1,40 +1,16 @@
 <html>
 	<head>
 		<?php include 'head.php' ?>
-		<link rel="stylesheet" type="text/css" href="decks.css">
+		<link rel="stylesheet" type="text/css" href="/css/decks.css">
 	</head>
 	<body>
 		<?php include 'header.php' ?>
 		<main class="content">
-			<h2 class="section-title">Decks</h2>
-			<div class="section section-decks">
-				<div class="section-background">
-					<div class="section-horz-3rd">
-						<div class="section-vert-split">
-							<div class="cover-image" data-image="images/brochure/320/aftdeck.png" title="view from the aft deck"></div>
-						</div>
-						<div class="section-vert-split">
-							<div class="cover-image" data-image="images/brochure/512/cruisingbow.png" title="cruising on the bow"></div>
-						</div>
+			<div class="decks-sections">
+				<div class="decks-text">
+					<div class="decks-title">
+						<h2 class="page-title">Decks</h2>
 					</div>
-					<div class="section-horz-3rd">
-						<div class="section-vert-split">
-							<div class="cover-image" data-image="images/brochure/320/sundeck.png" title="the aft deck at night"></div>
-						</div>
-						<div class="section-vert-split">
-							<div class="cover-image" data-image="images/brochure/512/waverunner.png" title="riding the waverunner"></div>
-						</div>
-					</div>
-					<div class="section-horz-3rd">
-						<div class="section-vert-split">
-							<div class="cover-image" data-image="images/brochure/320/sundeck-b.png" title="aft deck seating"></div>
-						</div>
-						<div class="section-vert-split">
-							<div class="cover-image" data-image="images/brochure/320/jacuzzipromotional.png" title="relaxing in the jacuzzi"></div>
-						</div>
-					</div>
-				</div>
-				<div class="cover-text cover-text-circle">
 					<p>
 						Starting aft, the newly designed and extended aft deck is the perfect spot
 						for al fresco dining. Stairs that run the full width of the yacht descend
@@ -42,6 +18,34 @@
 						The stairs also allow easy access to swimming, water sports, and boarding
 						the tenders and wave runners.
 					</p>
+				</div>
+				<div class="decks-images">
+					<div class="image-row">
+						<div id="aftdeck" class="full-width">
+							<img src="images/brochure/full/aftdeck.png" title="view from the aft deck"></img>
+						</div>
+					</div>
+					<div class="image-row">
+						<div id="sundeck-a" class="half-width">
+							<img src="images/brochure/full/sundeck-a.png" title="seating on the sundeck"></img>
+						</div>
+						<div id="sundeck-b" class="half-width">
+							<img src="images/brochure/full/sundeck-b.png" title="bar and a view from the sundeck"></img>
+						</div>
+					</div>
+					<div class="image-row">
+						<div id="sundecklounge" class="full-width">
+							<img src="images/brochure/full/sundecklounge.png" title="private dining on the sundeck"></img>
+						</div>
+					</div>
+					<div class="image-row">
+						<div id="cruisingbow" class="half-width">
+							<img src="images/brochure/512/cruisingbow.png" title="cruising on the bow"></img>
+						</div>
+						<div id="jacuzziempty" class="half-width">
+							<img src="images/brochure/full/jacuzziempty.png" title="jacuzzi on the bow"></img>
+						</div>
+					</div>
 				</div>
 			</div>
 		</main>
