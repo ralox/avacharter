20f0dff8eeb723aafc41283037df660320529948
David Barnett <dbarnett@liquidweb.com>
2015-10-18 16:09:23 -0400

    combine master and guest into just 'cabins'

---
+++ cabins.php
@@ -0,0 +1,64 @@
+<html>
+	<head>
+		<?php include 'head.php' ?>
+		<link rel="stylesheet" type="text/css" href="/css/cabins.css">
+	</head>
+	<body>
+		<?php include 'header.php' ?>
+		<main class="content flex-sections">
+			<div class="text">
+				<div class="title">
+					<h2 class="page-title">Cabins</h2>
+				</div>
+				<h3 class="section-title">Master Suite</h3>
+				<p class="fancy-letter">
+					The Ava sleeps eleven guests in five cabins, all with en suite bathrooms, including
+					four full size bathtubs with shower.
+
+					The large master suite on the main deck, with larger than king-size bed,
+					has a separate dressing room/office which can convert to a children's cabin.
+					Each cabin is fitted with large wardrobe, storage, TV, DVD, safe and stereo system.
+				</p>
+				<h3 class="section-title">Guest Suites</h3>
+				<p class="fancy-letter">
+					A second and third suite have large double beds, one of which has an extra hidden
+					Pullman berth. Both feature spacious bathrooms anointed with teakwood and marble.
+					Offered in complement are two twin cabins. Each cabin contains two comfortable,
+					large single beds and huge connecting bathrooms in marble.
+				</p>
+				
+				<div class="continue-tour">
+					<a href="services.php">learn about our services</a>
+				</div>
+			</div>
+			<div class="images">
+				<div class="image-row">
+					<div id="master-bed" class="full-width">
+						<img src="images/brochure/full/mastersuite.png" title="the master suite"></img>
+					</div>
+				</div>
+				<div class="image-row">
+					<div id="master-bath" class="half-width">
+						<img src="images/brochure/full/maincabinbath.png" title="master bath"></img>
+					</div>
+					<div id="guest-bath" class="half-width">
+						<img src="images/brochure/full/guestcabinbath.png" title="guest bath"></img>
+					</div>
+				</div>
+				<div class="image-row">
+					<div id="queen-cabin" class="full-width">
+						<img src="images/brochure/full/queencabin.png" title="guest suite with double bed"></img>
+					</div>
+				</div>
+				<div class="image-row">
+					<div id="twin-cabin" class="full-width">
+						<img src="images/brochure/full/twincabin.png" title="twin cabin with two single beds"></img>
+					</div>
+				</div>
+			</div>
+		</main>
+		<footer class="footer">
+			<?php include 'footer.php' ?>
+		</footer>
+	</body>
+</html>
