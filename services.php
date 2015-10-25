f5022ffc2f3e1b45200dd14a2b76c9c1e47ec05e
David Barnett <dbarnett@liquidweb.com>
2015-10-24 22:09:12 -0400

    services bare file prep

---
+++ services.php
@@ -0,0 +1,55 @@
+<html>
+	<head>
+		<?php include 'head.php' ?>
+		<link rel="stylesheet" type="text/css" href="css/services.css">
+	</head>
+	<body>
+		<?php include 'header.php' ?>
+		<main class="content">
+			<div class="title">
+				<h2 class="page-title">Services</h2>
+			</div>
+			<div class="text">
+				<P class="fancy-letter">
+					Ava's excellent entertaining areas, unique conference features, and the
+					captain's flexibility, effort, and experience will make your corporate and
+					event charters enjoyable and successful.  Able to entertain 49 guests at sea
+					and as many as 130 in port, the Ava is unique for corporate, conference, event,
+					and personal entertaining. Using a combination of the yacht and tents adjoining
+					quayside in the Monaco Old Port, Ava allows flexibility for an unlimited number
+					of guests for cocktails and sit-down catering.
+				</P>
+				<P>
+					The huge sun deck with covered lounge has a comfortable capacity of more than 50
+					persons. In addition to being a recreation area for lounging, sunbathing, and
+					water sports, the versatile aft deck is capable of accomodating 18 persons for
+					silver service dining. Another option is the formal dining room adjacent to the
+					saloon for 12 persons in elegance. Catering is provided by our chef "a place".
+					During winter months, and in case of bad weather, tents with heaters can be
+					mounted over the sun deck.
+				</P>
+				<p>
+					Our chef offers a variety of top quality buffets.  The seven professional crew
+					on board can be supplemented by additional entertainment and staff i.e.
+					bartenders, waiters, hostesses, guides, musicians, disc jokeys, photographers,
+					magicians, etc...
+				</p>
+			</div>
+			<div>
+				<ul class="watersports">
+					<h4 class="list-title">Water Sports</h4>
+					<li>21' Novurania semi rigid speedboat with 115hp 4 stroke Yamaha outboard engine</li>
+					<li>14' inflatable</li>
+					<li>2 x Yamaha 800cc waverunners</li>
+					<li>Waterskis</li>
+					<li>Biscuits</li>
+					<li>Snorkelling Gear</li>
+					<li>Fishing Equipment</li>
+				</ul>
+			</div>
+		</main>
+		<footer class="footer">
+			<?php include 'footer.php' ?>
+		</footer>
+	</body>
+</html>
