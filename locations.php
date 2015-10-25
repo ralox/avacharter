b461e1ac203438e86fcfa97732ae8c7bb8838b10
David Barnett <dbarnett@liquidweb.com>
2015-10-24 22:08:37 -0400

    locations rough layout

--- locations.php
+++ locations.php
@@ -1,21 +1,80 @@
 <html>
 	<head>
 		<?php include 'head.php' ?>
-		<link rel="stylesheet" type="text/css" href="locations.css">
+		<link rel="stylesheet" type="text/css" href="css/locations.css">
 	</head>
 	<body>
 		<?php include 'header.php' ?>
 		<main class="content">
-			<h2 class="page-title">Locations</h2>
-			<h3 class="page-tagline">
-					Ava has the best berthing locations in the ports of 
-					<span class="location-tag">Monaco</span>,
-					<span class="location-tag">Nice</span>, 
-					<span class="location-tag">Cannes</span>,
-					and 
-					<span class="location-tag">St Tropez</span>.
-			</h3>
-			<div class="section">
+			<div class="banner flex">
+				<div class="image-row">
+					<div id="map">
+						<img src="images/locations/cotedazur.jpg" height="200px" title="Monaco on a map"></img>
+					</div>
+				</div>
+				<div class="text intro">
+					<div class="title">
+						<div class="page-tagline">the beautiful côte d'azur</div>
+						<h2 class="page-title">Locations</h2>
+					</div>
+					<p class="fancy-letter">
+						Ava has the best berthing locations in the ports of Monaco, Cannes, Nice, and St Tropez.
+					</p>
+				</div>	
+			</div>
+			<div class="location-port-windows">
+				<div class="port-window thumbnail-"></div>
+				<div class="port-window thumbnail-"></div>
+				<div class="port-window thumbnail-"></div>
+				<div class="port-window thumbnail-"></div>
+			</div>
+			<div class="images">
+				<div class="image-row">
+					<div id="monaco" class="full-width">
+						<img src="images/locations/montecarlo-grandprix.jpg" title="Port Hercule setup for the Monaco Grand Prix"></img>
+					</div>
+				</div>
+			</div>
+			<div class="text flex">
+				<div class="location-text">
+					<h3>Monaco</h3>
+					<p>
+						Prime berthing in the old port of Monaco guarantees exceptional access for
+						conferences and events.  For the exciting and popular Monaco Grand Prix
+						Formula 1 car race, berthing is virtually on the racetrack.
+					</p>
+				</div>
+				<div class="location-text">
+					<h3>Cannes</h3>
+					<p>
+						In Cannes, the Ava is placed in front of the Palais du Festival conference center 
+						for premier access to Midem, Mipim, GSM, Miptv, Film Festival, Mipcom, Tax Free, 
+						and other world-class events and festivals.
+					</p>
+				</div>
+				<div class="location-text">
+					<h3>St.Tropez</h3>
+					<p>
+						The Old Port in St Tropez is also a prime location in front of Senequier/Cafe de Paris.
+					</p>
+				</div>
+			</div>
+			<div class="images wedge">
+				<div class="image-row">
+					<div id="nice" class="half-width">
+						<img src="images/locations/nice-sky.jpg" title="aerial view of Nice"></img>
+					</div>
+					<div id="sttropez" class="half-width">
+						<img src="images/locations/sttropez-beach.jpg" title="beach near St. Tropez"></img>
+					</div>
+				</div>
+				<div class="image-row">
+					<div id="cannes" class="full-width">
+						<img src="images/locations/cannes-fireworks.jpg" title="fireworks over Cannes"></img>
+					</div>
+				</div>
+			</div>
+			
 				<!--Google Street View: Monaco, 2011
 				<iframe id="google-streetview"
 					frameborder="0"
@@ -25,30 +84,6 @@
 					src="https://maps.google.com/maps?hl=en&amp;safe=off&amp;q=monte+carlo&amp;ie=UTF8&amp;hq=&amp;hnear=Monte+Carlo,+Monaco&amp;gl=us&amp;t=h&amp;layer=c&amp;cbll=43.736733,7.422178&amp;panoid=4CFR0C5twwSBIUZ8D8hc2Q&amp;cbp=13,150.67,,0,16.8&amp;source=embed&amp;ll=43.729755,7.422209&amp;spn=0.024437,0.060081&amp;z=14&amp;output=svembed">
 				</iframe>
 				-->
-				<div class="location-highlight location-highlight-monaco">
-					<p>
-						Prime berthing in the old port of Monaco guarantees exceptional access for
-						conferences and events.
-					</p>
-					<p>
-						For the exciting and popular Monaco Grand Prix
-						Formula 1 car race, berthing is virtually on the racetrack.  
-					</p>
-				</div>
-				<div class="location-highlight location-highlight-cannes">
-					<p>
-						In Cannes, the Ava is placed in front of the Palais du Festival conference center 
-						for premier access to Midem, Mipim, GSM, Miptv, Film Festival, Mipcom, Tax Free, 
-						and other world-class events and festivals.
-					</p>
-				</div>
-				<div class="location-highlight location-highlight-sttropez">
-					<p>
-						The Old Port in St Tropez is also a
-						prime location in front of Senequier/Cafe de Paris.
-					</p>
-				</div>
-			</div>
 		</main>
 		<footer class="footer">
 			<?php include 'footer.php' ?>
