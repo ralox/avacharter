086b62732cd63c60428b13a807ab1daabdabfc5f
David Barnett <dbarnett@liquidweb.com>
2015-11-22 14:33:45 -0500

    cabins updated

--- cabins.php
+++ cabins.php
@@ -7,20 +7,44 @@
 		<?php include 'header.php' ?>
 		<main class="content flex-sections">
 			<div class="text">
-				<div class="title">
+				<div class="title bottom-left-bracket">
+					<div class="page-tagline">luxury accomodations</div>
 					<h2 class="page-title">Cabins</h2>
 				</div>
+				<div class="dark-text-box">
+					<p class="fancy-letter">
+						The Ava sleeps eleven guests in five cabins, all with en suite bathrooms, including four full size bathtubs with shower.
+					</p>
+				</div>
+				<div class="cabin-specs">
+					<ul class="specs">
+						<li><div class="list-item-label">Cabins</div>
+							<ul class="list-item-value">
+								<li>3 Double</li>
+								<li>2 Twin</li>
+								<li>1 Convertible</li>
+							</ul>
+						</li>
+					</ul>
+					<ul class="specs">
+						<li><div class="list-item-label">Beds</div>
+							<ul class="list-item-value">
+								<li>1 Pullman</li>
+								<li>1 King</li>
+								<li>2 Queen</li>
+								<li>5 Single</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
 				<h3 class="section-title">Master Suite</h3>
-				<p class="fancy-letter">
-					The Ava sleeps eleven guests in five cabins, all with en suite bathrooms, including
-					four full size bathtubs with shower.
-
+				<p class="air-out">
 					The large master suite on the main deck, with larger than king-size bed,
 					has a separate dressing room/office which can convert to a children's cabin.
 					Each cabin is fitted with large wardrobe, storage, TV, DVD, safe and stereo system.
 				</p>
 				<h3 class="section-title">Guest Suites</h3>
-				<p class="fancy-letter">
+				<p class="air-out">
 					A second and third suite have large double beds, one of which has an extra hidden
 					Pullman berth. Both feature spacious bathrooms anointed with teakwood and marble.
 					Offered in complement are two twin cabins. Each cabin contains two comfortable,
